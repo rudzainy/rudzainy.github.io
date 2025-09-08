25dbfd635dceb35fd7ac6cd28faa6fbc5b444bfb
Rudzainy Rahman <rudzainy@gmail.com>
2025-09-08 14:38:47 +0800

    Update jei-es.js

--- jei-es.js
+++ jei-es.js
@@ -118,3 +118,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const ansArray = [
+	"true","correct","yes","right","accurate","valid",
+	"betul","sungguh","ya","benar","tepat","haah","ha'ah",
+	"yup","yups","y","t","1","em","yea",
+	"ran'galhu","rangalhu","kanaaiy","munaasibu","aan","aan'","saḩḩa","sahha"
+];
+
+let hackering = () => {
+  jawaban = document.getElementById("answer").value;
+  console.log(jawaban);
+  if(ansArray.includes(jawaban.toLowerCase())) {
+    const title = "The Doorway <i>is Opened!</i>";
+    let modalTitle = document.getElementById('takdeeModalLabel');
+    modalTitle.innerHTML = title;
+    let box = document.getElementById('box');
+    box.innerHTML = "Congrtulations code breaker person! 👀<br />Here are some goodies for you:<br /><ol><li><a href='/docs/'>Some documents</a></li><li>...</li><li>Unlisted YouTube videos:<ul><li><a href='https://youtu.be/haeEC00aPTM'>Maya x Rudzainy Solemnization</a></li><li><a href='https://youtu.be/VVevtD-_F8Y'>Test link</a></li></ul></li><li><a href='https://rudzainy.blogspot.com/'>Old blog</a></li></ol><br /><h2>Thinking of a name for this section 🤔</h2><ul><li><a href='/posts/rails-7-dropdown-image'>1</a></li><li>Fun fact: you can also access this website at <a href='https://rudzainy.github.io'>rudzainy.github.io</a></li><li>3</li><li>4</li></ul>";
+  };
+};
